a0e92b7a144d7f9b7054d85fc4dae72bbde47188
syeda <sbtshakeel61@gmail.com>
2024-06-01 00:02:03 +0500

    first commit

---
+++ index.ts
@@ -0,0 +1,10 @@
+//type unknown
+
+let val1 : unknown;
+val1 = 10
+val1 = "string"
+val1 = true
+val1 = undefined
+val1 = null
+
+//let val4 : string = val1  //{commentout} //error val4 because type unknown is not assignable to string
